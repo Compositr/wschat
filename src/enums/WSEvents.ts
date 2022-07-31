f2e2e5bf58d5bbea0c48fb8d3b82d968413b942f
Jim <compositr@compositr.dev>
2022-07-31 19:37:23 +1000

    feat:  new ws events
    Signed-off-by: Jim <compositr@compositr.dev>

--- src/enums/WSEvents.ts
+++ src/enums/WSEvents.ts
@@ -1,6 +1,8 @@
 const enum WSEvents {
   MESSAGE = "CHAT_MESSAGE",
-  NEW_PEER = "NEW_PEER"
+  NEW_PEER = "NEW_PEER",
+  LIST_NICKS = "LIST_NICKS",
+  NICK_RESPONSE = "NICK_RESPONSE",
 }
 
 export default WSEvents;
